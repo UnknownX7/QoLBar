8eab583164ad5ef4daca5bee157874ef4361dd50
UnknownX <jaguar51896@gmail.com>
2021-01-01 10:59:06 -0600

    Remove button transparency

--- BarUI.cs
+++ BarUI.cs
@@ -227,6 +227,7 @@ namespace QoLBar
             {
                 ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 0);
                 ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0);
+                ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0.286f, 0.286f, 0.286f, 1f));
 
                 if (docked)
                     ImGui.SetNextWindowPos(barPos, ImGuiCond.Always, piv);
@@ -274,6 +275,7 @@ namespace QoLBar
 
                 ImGui.End();
 
+                ImGui.PopStyleColor();
                 ImGui.PopStyleVar(2);
             }
 
