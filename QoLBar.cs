7c392da440875d23c20c23407f1b0e843d7ffc55
UnknownX <jaguar51896@gmail.com>
2021-05-05 23:45:18 -0500

    Bump version

--- QoLBar.cs
+++ QoLBar.cs
@@ -18,7 +18,7 @@ using QoLBar.Attributes;
 
 // I'm too lazy to make a file just for this
 [assembly: AssemblyTitle("QoLBar")]
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
 
 // Disclaimer: I have no idea what I'm doing.
 namespace QoLBar
